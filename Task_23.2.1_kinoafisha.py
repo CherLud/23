689d95aedc618e56975e6adfd47ed9f159f0f6c2
CherLud <Kushnarova7893@mail.ru>
2025-03-17 12:20:10 +0300

    Add files via upload

---
+++ Task_23.2.1_kinoafisha.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from operator import itemgetter
+
+url = f'https://www.kinoafisha.info/online/movies/'
+html_content = requests.get(url).text
+
+soup = BeautifulSoup(html_content, 'lxml')
+
+entries = soup.find_all('div', class_='movieList_item movieItem movieItem-grid grid_cell3')
+data = []
+for entry in entries:
+    td_film_details = entry.find('div', class_='movieItem_info')
+    film_name = td_film_details.find('a').text
+
+    film_genre = entry.find('span', class_='movieItem_genres').text
+
+    release_date = entry.find('span', class_='movieItem_year').text
+
+    rating = entry.find('span', class_='mark_num').text
+
+    data.append({'film_name': film_name, 'film_genre': film_genre, 'release_date': release_date, 'rating': rating})
+#print(data)
+
+data1 = sorted(data, key=itemgetter('rating'), reverse=True)
+print(f'ТОП 20 фильмов с высшим рейтингом:\n {data1[:20]}')
+df = pd.DataFrame(data1[:20])
+df.to_excel('user_rates5.xlsx')
